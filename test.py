8671c8264de0f45a813c8252bda8ee8b11e45a76
M <goldb3rg@users.noreply.github.com>
2015-04-15 13:30:01 +0100

    Updated to extract html link

--- test.py
+++ test.py
@@ -6,8 +6,11 @@ driver.get("http://www.skybet.com/football/coupon/all-matches-by-day")
 assert "All Matches By Day" in driver.title
 elem = driver.find_elements_by_class_name("all-bets-link")
 
+linkstr = ""
 for items in elem:
-    print items.text
-
+    #print items.text
+    linkstr = item.get_attribute('href')
+    print linkstr
+    
 assert "No results found." not in driver.page_source
 driver.close()
